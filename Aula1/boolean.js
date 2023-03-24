2d287e796fc7090b77d25619e88b93b58319978c
Rednuhtkcalb14 <danielgco3465@gmail.com>
2023-03-24 02:24:11 -0300

    adicionando a aula sobre booleanos

---
+++ Aula1/boolean.js
@@ -0,0 +1,10 @@
+// Valores booleanos
+/*
+    Valore usados principalmente para comapração
+como por exemplo validações de senhas de bancos
+*/
+
+console.log(0 === 0)
+console.log(0 == "0")
+console.log(0 === "0")
+console.log(0 === null)
